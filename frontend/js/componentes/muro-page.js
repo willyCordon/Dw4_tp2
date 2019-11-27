e82abd9a5a9ab9d01d60c0559f8ec3882b3c8ece
Alejandro <alejandro.didonato@davinci.edu.ar>
2019-11-26 22:24:44 -0300

    estilos

--- frontend/js/componentes/muro-page.js
+++ frontend/js/componentes/muro-page.js
@@ -1,10 +1,10 @@
 Vue.component('muro-page',{
     template: `<div>  
        <div class='row'>
-            // <div class='col-xs-12 col-md-12'>
-            //     <muro-page-equipos>
-            //     </muro-page-equipos>
-            // </div>
+            <div class='col-xs-12 col-md-12'>
+            <muro-page-equipos>
+             </muro-page-equipos>
+            </div>
             <div class='col-xs-12 col-md-12'>
                 <muro-page-listado>
                 </muro-page-listado>
